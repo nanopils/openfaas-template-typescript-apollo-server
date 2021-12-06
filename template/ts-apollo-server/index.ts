3eb20aea829f5da9d90d05eb6b4b1956bfd82d97
Nils-Frederic Perret <nils-frederic.perret@bos.no>
2021-12-06 17:46:01 +0100

    Change from Apollo CLI to Rover and make the start sequence async

--- template/ts-apollo-server/index.ts
+++ template/ts-apollo-server/index.ts
@@ -12,6 +12,8 @@ import { buildFederatedSchema } from './buildFederatedSchema';
 import { authChecker } from './function/auth';
 import resolvers, { orphanedTypes, federationResolvers, getEnvironmentVariables } from './function/resolvers';
 
+let vars: { [key: string]: any } = null;
+
 const app = express();
 
 const port = process.env?.PORT || 3000;
@@ -21,35 +23,43 @@ const introspection = !!process.env?.APOLLO_INTROSPECTION ? process.env?.APOLLO_
 const playground = !!process.env?.APOLLO_PLAYGROUND ? process.env?.APOLLO_PLAYGROUND === 'true' : false;
 const debugSchemaPath = path.resolve(__dirname, 'schema.gql');
 
+const loadEnvVariables = async () => {
+  const hasFunction = typeof getEnvironmentVariables === 'function';
+  if (hasFunction) {
+    return getEnvironmentVariables(process.env).then(variables => {
+      vars = variables;
+      return startServer();
+    });
+  }
+  vars = { ...process.env };
+  return startServer();
+};
+
 const updateApolloStudioSubgraph = async () => {
-  const vars: { [key: string]: any } =
-    typeof getEnvironmentVariables === 'function' ? await getEnvironmentVariables(process.env) : { ...process.env };
   const apolloKey = vars?.APOLLO_KEY;
-  const apolloGraphVariant = vars?.APOLLO_GRAPH_VARIANT;
+  const apolloGraphRef = vars?.APOLLO_GRAPH_REF;
   const apolloSchemaReporting = vars?.APOLLO_SCHEMA_REPORTING;
 
   if (!!apolloKey) {
     const serviceName = vars?.OPENFAAS_SERVICE_NAME || null;
     const functionName = vars?.OPENFAAS_FUNCTION_NAME || null;
-    const supergraphName = vars?.APOLLO_STUDIO_SUPERGRAPH_NAME || null;
-    if (!apolloGraphVariant || !serviceName || !supergraphName) {
+    if (!apolloGraphRef || !serviceName) {
       console.error(`You should provide the following in order to update the Apollo Studio subgraph:
         - the OpenFAAS function name
-        - an Apollo Studio variant name
-        - an Apollo Studio supergraph name
+        - an Apollo Studio ref (APOLLO_GRAPH_REF)
       `);
       return;
     }
     const routingUrl = `http://${functionName}:8080/graphql`;
+    const introspectionUrl = 'http://localhost:8080/graphql';
     console.log(`Uploading the GraphQL schema of the ${serviceName} service to Apollo Studio`);
     exec(
-      `npx apollo service:push \
-        --graph="${supergraphName}" \
-        --key="${apolloKey}" \
-        --variant="${apolloGraphVariant}" \
-        --serviceName="${serviceName}" \
-        --serviceURL="${routingUrl}" \
-        --endpoint="http://localhost:8080/graphql"`,
+      `rover subgraph introspect ${introspectionUrl} | \
+        APOLLO_KEY=${apolloKey} \
+        rover subgraph publish ${apolloGraphRef} \
+        --name ${functionName} \
+        --routing-url ${routingUrl} \`
+        --schema -`,
       (err, stdout, stderr) => {
         if (err) {
           //some err occurred
@@ -61,7 +71,7 @@ const updateApolloStudioSubgraph = async () => {
         }
       },
     );
-    console.log(`Updated Apollo Studio schema!`, routingUrl, apolloGraphVariant, apolloSchemaReporting);
+    console.log(`Updated Apollo Studio schema!`, routingUrl, apolloGraphRef, apolloSchemaReporting);
   } else {
     console.log(
       `The APOLLO_KEY environment variable was not defined, so the schema was not uploaded to Apollo Studio.`,
@@ -73,7 +83,7 @@ const updateApolloStudioSubgraph = async () => {
   }
 };
 
-async function startServer() {
+const startServer = async () => {
   const schema = await buildFederatedSchema(
     {
       // @ts-ignore
@@ -103,6 +113,6 @@ async function startServer() {
     console.log(`🚀 OpenFaaS GraphQL listening on port: ${port}`);
     await updateApolloStudioSubgraph();
   });
-}
+};
 
-startServer().catch(error => console.log(error));
+loadEnvVariables().catch(error => console.log(error));
