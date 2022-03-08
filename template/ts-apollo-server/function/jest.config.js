2eceba7a8b7581eafad05c68c3c8ebef3e0346b8
Nils Perret <nils-frederic.perret@bos.no>
2022-03-08 10:02:20 +0100

    Add the possibility to run tests locally outside of Docker

--- template/ts-apollo-server/function/jest.config.js
+++ template/ts-apollo-server/function/jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  setupFiles: ['dotenv/config'],
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['.yalc', 'node_modules'],
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
